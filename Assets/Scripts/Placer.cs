15ee3926126cd0a41c2b9cbbbb33a152e2783443
Claire Meyer <signedrewind@gmail.com>
2019-12-20 23:04:16 -0800

    Added selector

--- Assets/Scripts/Placer.cs
+++ Assets/Scripts/Placer.cs
@@ -9,24 +9,29 @@ public class Placer : MonoBehaviour
     [SerializeField]
     private Tilemap _tilemap;
 
-    [SerializeField]
-    private SpriteRenderer _ghostTile;
-
     private Tile _prevTile;
-    
-    private void Awake()
-    {
-        
-    }
+
+    [SerializeField]
+    private Tile _selectorTile;
+
+    private Vector3Int _selectorPos;
     
     void Update()
     {
         var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var tilePos =  _tilemap.LocalToCell(worldPos);
+
         tilePos.z = 0;
-        tilePos.x -= 6;
-        tilePos.y -= 6;
-        Debug.Log(tilePos);
-        _tilemap.SetTile(tilePos, null);
+        tilePos.x -= 7;
+        tilePos.y -= 5;
+        
+        if (_tilemap.GetTile<Tile>(tilePos) == null) return;
+        
+        tilePos.z = 1;
+        
+        _tilemap.SetTile(_selectorPos, _prevTile);
+        _prevTile = _tilemap.GetTile<Tile>(tilePos);
+        _tilemap.SetTile(tilePos, _selectorTile);
+        _selectorPos = tilePos;
     }
 }
